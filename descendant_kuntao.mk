bc7372d4ac68fd674fe98fc2ec607d03a9287b2e
Anis Noorudheen <35500897+anisnoorudheen@users.noreply.github.com>
2019-06-16 15:40:47 +0530

    Update and rename lineage_kuntao.mk to descendant_kuntao.mk

--- descendant_kuntao.mk
+++ descendant_kuntao.mk
@@ -26,13 +26,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/descendant/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lenovo/kuntao/device.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_kuntao
+PRODUCT_NAME := descendant_kuntao
 PRODUCT_DEVICE := kuntao
 PRODUCT_BRAND := Lenovo
 PRODUCT_MODEL := Lenovo P2a42
